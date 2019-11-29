93a7e8cb9b23e5f6aa1f01c614fb5c160df962fe
ygjoe <502253898@qq.com>
2019-11-29 17:23:24 +0800

    添加滑动加载更多

--- src/js/getMoreLoad.js
+++ src/js/getMoreLoad.js
@@ -1,6 +1,33 @@
+// 配置项
+let options = {
+  dom: window, // 绑定滑动的dom
+  model: 'top', // 滑动模式，top = 上滑；left = 左滑
+  distance: 0, // 距离边缘的距离触发回调函数
+  callback: () => {} // 滑动到末尾回调函数
+}
 
+function init (option) {
+  setOption(option)
+  addListener()
+}
 
-function init(){
+// 设置配置项
+function setOption (option = {}) {
+  options = { ...options, ...option }
+}
+
+// 监听滚动
+function addListener () {
+  options.dom.addEventListener('scroll')
+}
+
+// 上滑加载
+function getMoreTop () {
+
+}
+
+// 左滑加载
+function getMoreLeft () {
 
 }
 
